75c97f177528ca93f7d961a868622b73907e6c51
anderson3501 <116132709+anderson3501@users.noreply.github.com>
2024-04-04 19:07:30 -0500

    Add files via upload

---
+++ distribuidos/src/main/java/com/example/distribuidos/controller/PersonController.java
@@ -0,0 +1,16 @@
+package com.example.distribuidos.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @GetMapping()    
+    public String Person() {
+        return "Prueba proy 2";
+    }
+    
+}
